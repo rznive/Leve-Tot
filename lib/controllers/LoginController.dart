7b688e5c66484626f35f4635b7098fb2e070c921
mrzptra <rezaofc.jr@gmail.com>
2024-11-21 11:35:59 +0700

    adding Integrasi API for Login

---
+++ lib/controllers/LoginController.dart
@@ -0,0 +1,31 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+class LoginController {
+  static Future<String> login(String email, String password) async {
+    const String apiUrl = "http://103.127.132.23/api/signin/";
+
+    try {
+      final response = await http.post(
+        Uri.parse(apiUrl),
+        headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+        body: {
+          'email': email,
+          'password': password,
+        },
+      );
+
+      final responseData = jsonDecode(response.body);
+      if (responseData['status'] == true) {
+        print("Login Berhasil: ${responseData['user']}");
+        return "Login Berhasil: ${responseData['user']}";
+      } else {
+        print("Login Gagal: ${responseData['error']}");
+        return "Login Gagal: ${responseData['error']}";
+      }
+    } catch (e) {
+      print("Terjadi kesalahan pada saat login: $e");
+      return "Terjadi kesalahan pada saat login: $e";
+    }
+  }
+}
